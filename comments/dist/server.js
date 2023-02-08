b603dceb46786c9d3baf99731b187e953f9fa603
eliyahukoren <elik.koren@gmail.com>
2023-02-08 12:06:28 +0200

    fixing comments service

--- comments/dist/server.js
+++ comments/dist/server.js
@@ -4,10 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createCommentsServer = void 0;
+const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
 const routes_1 = require("./routes");
 const createCommentsServer = () => {
     const app = (0, express_1.default)();
+    const allowedOrigins = ["http://localhost:3000"];
+    const options = {
+        origin: allowedOrigins,
+    };
+    app.use((0, cors_1.default)(options));
     app.use(express_1.default.urlencoded({ extended: true }));
     app.use(express_1.default.json());
     app.use("/", routes_1.commentRoute);
--- comments/server.ts
+++ comments/server.ts
@@ -1,9 +1,16 @@
+import cors from 'cors';
 import express, { Express } from "express";
 import { commentRoute } from "./routes";
 
 const createCommentsServer = (): Express => {
   const app: Express = express();
 
+  const allowedOrigins = ["http://localhost:3000"];
+  const options: cors.CorsOptions = {
+    origin: allowedOrigins,
+  };
+
+  app.use(cors(options));
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
